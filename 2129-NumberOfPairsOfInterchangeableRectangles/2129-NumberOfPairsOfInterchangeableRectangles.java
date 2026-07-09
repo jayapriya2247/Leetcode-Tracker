01417ef949a6ab0e6a4ccbce3dcf033ceb1b8fdb
jayapriya2247 <jayapriyatnp06@gmail.com>
2026-07-09 15:06:26 +0530

    Create 2129-NumberOfPairsOfInterchangeableRectangles

---
+++ 2129-NumberOfPairsOfInterchangeableRectangles/2129-NumberOfPairsOfInterchangeableRectangles.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 3:06:48 PM
+class Solution {
+    public long interchangeableRectangles(int[][] rectangles) {
+       long res = 0;
+       Map<Double,Integer>hm = new HashMap();
+       for(int[] r:rectangles){
+        double ratio = (double) r[0] / r[1];
+        res = res+hm.getOrDefault(ratio,0);
+        hm.put(ratio,hm.getOrDefault(ratio,0) +1);
+       }
+       return res;
+    }
+}
