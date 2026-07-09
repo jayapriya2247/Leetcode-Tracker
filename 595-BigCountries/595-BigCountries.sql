f2e2df471e39f7e76d6bef042201ef5abdb046a1
jayapriya2247 <jayapriyatnp06@gmail.com>
2026-07-09 15:07:02 +0530

    Create 595-BigCountries

---
+++ 595-BigCountries/595-BigCountries.sql
@@ -0,0 +1,5 @@
+-- Last updated: 7/9/2026, 3:07:24 PM
+# Write your MySQL query statement below
+Select name , population , area
+from World
+where area >= 3000000 OR population >= 25000000;# Write your MySQL query statement below
